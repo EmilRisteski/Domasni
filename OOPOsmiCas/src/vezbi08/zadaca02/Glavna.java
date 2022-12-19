d58e5ac978ba2fe8399bef59da4eb9936aed02ae
Zaklina Risteska <emil.risteski2@gmail.com>
2022-12-19 18:08:59 +0100

    Osmi vezbi

---
+++ OOPOsmiCas/src/vezbi08/zadaca02/Glavna.java
@@ -0,0 +1,23 @@
+package vezbi08.zadaca02;
+
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Glavna {
+
+	public static void main(String[] args) {
+		try {
+			BufferedReader object =  new BufferedReader(new InputStreamReader(System.in));
+			String zbor = object.readLine();
+			
+			Recnik object1 = new Recnik();
+			object1.zborovi();
+			object1.prebaruvanje(zbor);
+		}
+		catch (IOException e) {
+			System.out.println(e.toString());
+		}
+	}
+
+}
---
+++ OOPOsmiCas/src/vezbi08/zadaca02/Recnik.java
@@ -0,0 +1,25 @@
+package vezbi08.zadaca02;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Recnik {
+	Map<String, String> recnik =  new HashMap<String, String>();
+	
+	public void zborovi() {
+		recnik.put("Word", "Zbor");
+		recnik.put("Door", "Vrata");
+		recnik.put("Glasses", "Naocari");
+		recnik.put("Pencil", "Moliv");
+		recnik.put("Book", "Kniga");
+	}
+		
+	public void prebaruvanje (String zbor) {
+		if(recnik.containsKey(zbor)) {
+			System.out.println(recnik.get(zbor));
+		}
+		else {
+			System.out.println("Toj zbor go nema vo recnikot.");
+		}
+	}
+}
